24660b15ef929553855400c9092261dccd957e6a
Anish Godse <memymyselfanish@gmail.com>
2026-02-24 16:47:37 +0530

    Initial commit

---
+++ CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        int a = 10;
+        int b = 20;
+        System.out.println("Addition:"+c.add(a, b));
+        System.out.println("Subtraction" + c.sub(a, b));
+    }
+}
